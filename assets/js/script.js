fa199f5864296d45b681c97ffe013701fcae9a97
Louw Johan Coetzee <Louwjohanc@gmail.com>
2021-12-26 22:02:30 +0000

    Add code to use function for all arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,30 +6,29 @@ let intermediate = [["The Good the Bad and the Ugly","For Time", "200 Double-Und
     "With a running clock, complete 3 rounds of the prescribed work in the order written as fast as possible (“For Time“)."], ["211221-Mayhem", "For Time",
     "1 Burpee", "2 Strict Pull-Ups", "3 Knees-to-Elbows", "4 Strict Handstand Push-Ups", "5 Box Jump Overs (24/20 in)", "6 Pistols", "7 Kipping Toes-to-Bars",
    "8 V-Ups", "9 second Handstand Hold", "10 Push-Ups", "11 Overhead Walking Lunges (2x45/25 lb)", "12 Dumbbell Thrusters (2x45/25 lb)",
-    "Start with 1 Burpee. Then do 2 Strict Pull-Ups and 1 Burpee. Then 3 Knees-to-Elbows, 2 Strict Pull-Ups, and 1 Burpee Continue this way until the final round of 12 Dumbbell Thrusters 11 Overhead Walking Lunges, and each movement descending in repetitions all the way down to 1 Burpee.With a running clock, complete the prescribed work in the order written as fast as possible (“For Time“)."] ];
+    "Start with 1 Burpee. Then do 2 Strict Pull-Ups and 1 Burpee. Then 3 Knees-to-Elbows, 2 Strict Pull-Ups, and 1 Burpee Continue this way until the final round of 12 Dumbbell Thrusters 11 Overhead Walking Lunges, and each movement descending in repetitions all the way down to 1 Burpee.With a running clock, complete the prescribed work in the order written as fast as possible (“For Time“)."],
+   ["Laps of Doom", "For Time (in a Team of 4)", "200 meter Sprint (each). Then, complete: 50 meter Battle Cancer Equipment Carry / Partner Hold", "Time Cap: 9 minutes", "With a running clock, complete 200 meter Run for each athlete. Once one athlete is done with the 200 meter Run, that athlete will tag the next athlete to do the 200 meter Sprint. Once all the team members are done with 200 meter Sprint, two athletes will work at a time to carry the Battle Cancer equipment until 50 meters is completed while the other partner is holding a Partner Carry.", 
+   "Score is the time on the clock when the 50 meter Weighted Carry is completed. With a running clock, complete 200 meter Run for each athlete. Once one athlete is done with the 200 meter Run, that athlete will tag the next athlete to do the 200 meter Sprint. Once all the team members are done with 200 meter Sprint, two athletes will work at a time to carry the Battle Cancer equipment until 50 meters is completed while the other partner is holding a Partner Carry." ] ];
 
-// function to create HTML syntax//
+// function to create HTML syntax for a list from an array//
 
-function createList(){
+function createList (arr) {
         let exTable = "<ol>";
-        for (let i = 0 ; i < intermediate.length; i++){
-            let listItem = `<li>${intermediate[i][0]}</li>`;
+        for (let i = 0 ; i < arr.length; i++){
+            let listItem = `<li id="list-item${i+1}">${arr[i][0]}</li>`;
             exTable += listItem
         }
         exTable + "</ol>";
         return exTable;
 }
 
+//Function HTML syntax created from an  array and inserts into DOM //
 
-//Function takes HTML syntax and inserts into DOM
-
-function displayList(){
-    let exList = createList();
+function displayList(arr){
+    let exList = createList(arr);
     return document.getElementById("display-exercise").innerHTML = exList;
 }
 
 let intermediateButton = document.getElementById("slide1-button");
 
-intermediateButton.addEventListener("click", displayList);
-
-console.log(intermediateButton);
+intermediateButton.addEventListener("click", function(){displayList(intermediate)});
