f0e6739a8c872512cec1277b7a3eb862d4e61aa3
Louw Johan Coetzee <Louwjohanc@gmail.com>
2021-12-28 22:12:04 +0000

    Add code for random button

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,28 +7,31 @@ let advanced = [["THE GOOD THE BAD AND THE UGLY","For Time", "200 Double-Unders"
     "1 Burpee", "2 Strict Pull-Ups", "3 Knees-to-Elbows", "4 Strict Handstand Push-Ups", "5 Box Jump Overs (24/20 in)", "6 Pistols", "7 Kipping Toes-to-Bars",
    "8 V-Ups", "9 second Handstand Hold", "10 Push-Ups", "11 Overhead Walking Lunges (2x45/25 lb)", "12 Dumbbell Thrusters (2x45/25 lb)",
     "Start with 1 Burpee. Then do 2 Strict Pull-Ups and 1 Burpee. Then 3 Knees-to-Elbows, 2 Strict Pull-Ups, and 1 Burpee Continue this way until the final round of 12 Dumbbell Thrusters 11 Overhead Walking Lunges, and each movement descending in repetitions all the way down to 1 Burpee.With a running clock, complete the prescribed work in the order written as fast as possible (“For Time“)."],
-   ["LAPS OF DOOM", "For Time (in a Team of 4)", "200 meter Sprint (each). Then, complete: 50 meter Battle Cancer Equipment Carry / Partner Hold", "Time Cap: 9 minutes", "With a running clock, complete 200 meter Run for each athlete. Once one athlete is done with the 200 meter Run, that athlete will tag the next athlete to do the 200 meter Sprint. Once all the team members are done with 200 meter Sprint, two athletes will work at a time to carry the Battle Cancer equipment until 50 meters is completed while the other partner is holding a Partner Carry.", 
-   "Score is the time on the clock when the 50 meter Weighted Carry is completed. With a running clock, complete 200 meter Run for each athlete. Once one athlete is done with the 200 meter Run, that athlete will tag the next athlete to do the 200 meter Sprint. Once all the team members are done with 200 meter Sprint, two athletes will work at a time to carry the Battle Cancer equipment until 50 meters is completed while the other partner is holding a Partner Carry."], ["THE RUNNING DEAD", "For Time", "600 meter Run",
+   ["LAPS OF DOOM", "For Time (in a Team of 4)", "200 meter Sprint (each). Then, complete: 50 meter Battle Cancer Equipment Carry / Partner Hold", "Time Cap: 9 minutes", "With a running clock, complete 200 meter Run for each athlete. Once one athlete is done with the 200 meter Run, that athlete will tag the next athlete to do the 200 meter Sprint. Once all the team members are done with 200 meter Sprint, two athletes will work at a time to carry the Battle Cancer equipment until 50 meters is completed while the other partner is holding a Partner Carry."], ["THE RUNNING DEAD", "For Time", "600 meter Run",
    "15 Kettlebell Devil Presses (2x70/53 lb)", "600 meter Run", "With a running clock, complete the prescribed work in the order written as fast as possible (“For Time“)."], ["OH MY SWEET LEGGINGS", "For Time", "20-16-12-8 calorie Row", "26 Pistols", "8-12-16-20 Overhead Squats (60/40 kg)",
    "With a running clock, complete the prescribed work in the order written as fast as possible (“For Time“). Athlete must complete 20 calorie Row, 26 Pistols, 8 Overhead Squats for the first round. For the second round, athlete will then perform 16 calorie Row, 26 Pistols, 12 Overhead Squats, etc.With a running clock, complete the prescribed work in the order written as fast as possible (“For Time“). Athlete must complete 20 calorie Row, 26 Pistols, 8 Overhead Squats for the first round. For the second round, athlete will then perform 16 calorie Row, 26 Pistols, 12 Overhead Squats, etc."],
-["12 DAYS OF DB CHRISTMAS", "For Time", "1 Dumbbell Turkish Get-Up (50/35 lb)", "2 Strict Toes-to-Bar", "3 Burpees Over the Dumbbell", "4 Dumbbell Power Snatches (alternating) (50/35 lb)", "5 Supermans", "6 Push-Ups", "7 Single Dumbbell Front Squats (50/35 lb)", "8 Single-Arm Dumbbell Hang Power Cleans (50/35 lb)", "9 Dumbbell Sumo Deadlifts (50/35 lb)", "10 Walking Lunge Steps (alternating)", "11 Strict Sit-Ups", "12 Dumbbell Swings (50/35lb)", "Start with 1 Turkish Get-Up (TGU). Then do 2 Strict Toes-to-Bar and 1 TGU. Then 3 Burpees Over the Dumbbell, 2 Strict Toes-to-Bar, and 1 TGU. Continue this way until the final round of 12 Dumbbell Swings, 11 Strict Sit-Ups, and each movement descending in repetitions all the way down to 1 TGU — for 364 repetitions total.",
-["IT", "3 Rounds for Time", "21 Burpees to Target", "15 Hang Squat Cleans (155/105 lb)", "9 Bar Muscle-Ups", "With a running clock, complete 3 rounds of the prescribed work in the order written as fast as possible (“For Time“)."]],
-["MOUNT KILIMANJARO", "For Time", "800 meter Run", "40 Box Step-Ups (30/24 in)", "600 meter Run", "30 Box Jump Overs (30/24 in)", "400 meter Run", "20 Box Step-Ups (30/24 in)", "200 meter Run", "10 Box Jump Overs (30/24 in)", "With a running clock, complete the prescribed work in the order written as fast as possible (“For Time“). "], ["ANTIPOLIS HOME WOD 46", "4 Rounds for Time", "10 Plank Rows (50/35 lb, each side)", "10 Skull Crushers (50/35 lb)",
-"10 Bulgarian Split Squats", "10 Single-Leg Romanian Deadlifts (50/35 lb)", "With a running clock, complete 4 rounds of the prescribed work in the order written."]]
- let beginner = [["Run for your life"]]
- let advancedButton = document.getElementById("slide2-button");
- let beginnerButton = document.getElementById("slide1-button");
+["12 DAYS OF DB CHRISTMAS", "For Time", "1 Dumbbell Turkish Get-Up (50/35 lb)", "2 Strict Toes-to-Bar", "3 Burpees Over the Dumbbell", "4 Dumbbell Power Snatches (alternating) (50/35 lb)", "5 Supermans", "6 Push-Ups", "7 Single Dumbbell Front Squats (50/35 lb)", "8 Single-Arm Dumbbell Hang Power Cleans (50/35 lb)", "9 Dumbbell Sumo Deadlifts (50/35 lb)", "10 Walking Lunge Steps (alternating)", "11 Strict Sit-Ups", "12 Dumbbell Swings (50/35lb)", "Start with 1 Turkish Get-Up (TGU). Then do 2 Strict Toes-to-Bar and 1 TGU. Then 3 Burpees Over the Dumbbell, 2 Strict Toes-to-Bar, and 1 TGU. Continue this way until the final round of 12 Dumbbell Swings, 11 Strict Sit-Ups, and each movement descending in repetitions all the way down to 1 TGU — for 364 repetitions total."],
+["IT", "3 Rounds for Time", "21 Burpees to Target", "15 Hang Squat Cleans (155/105 lb)", "9 Bar Muscle-Ups", "With a running clock, complete 3 rounds of the prescribed work in the order written as fast as possible (“For Time“)."],
+["MOUNT KILIMANJARO", "For Time", "800 meter Run", "40 Box Step-Ups (30/24 in)", "600 meter Run", "30 Box Jump Overs (30/24 in)", "400 meter Run", "20 Box Step-Ups (30/24 in)", "200 meter Run", "10 Box Jump Overs (30/24 in)", "With a running clock, complete the prescribed work in the order written as fast as possible (“For Time“). "]]
 
+let beginner = [["THE GHOST", "3 rounds of:", "1 minute of rowing", "1 minute of burpees", "1 minute of jump rope", "1 minute of rest"], ["LIGHT LINDA", "15-12-9-6-3 reps for time of:", "Deadlift, 65/95", "Bench press, 45/65", "Squat clean, 35/45"], ["HALF CHELSEA", "Every minute on the minute for 10 minutes perform:",
+"3 ring rows", "6 elevated push-ups", "9 squats","If you fall behind the clock, keep going for 10 minutes and see how many rounds you can complete."], ["EXTENDED ELIZABETH", "21-15-9 reps for time:", "Power cleans", "Jumping ring dips"], ["DOUBLE ISABEL", "For time:", "30 snatches"], ["NASTY GIRLS V2", "3 rounds for time of:", "25 squats", "5 assisted pull-ups", "10 hang power cleans"],
+["HOLLEYMAN", "10 rounds for time of:", "5 wall-ball shots", "3 push-ups", "1 power clean"], ["JERRY", "For time:", "Run 800 meters", "Row 1,000 meters", "Run 800 meters"], ["MAXTON", "5 rounds for time of:", "4 assisted pull-ups", "13 box step-ups", "11 burpees"], ["NATE", "Complete as many rounds as possible in 20 minutes of:", "4 assisted pull-ups", "4 assisted dips", "4 assisted handstand push-ups", "8 kettlebell swings"]]
+
+let advancedButton = document.getElementById("slide2-button");
+let beginnerButton = document.getElementById("slide1-button");
+let randomButton = document.getElementById("random-button");
+let timerButton = document.getElementById("timer-button")
  // function to create HTML syntax for a list from an array//
 
 function createList (arr) {
         let exTable = `<h3>Choose an exercise routine!</h3> <ol>`;
         for (let i = 0 ; i < arr.length; i++){
             if (arr === advanced){
-                let listItem = `<li><button class="advanced" id="list-item${i+1}">${arr[i][0]}</button></li>`;
+                let listItem = `<li><button id="adlist-item${i+1}">${arr[i][0]}</button></li>`;
                 exTable += listItem
             } else {
-                let listItem = `<li><button class="beginner" id="list-item${i+1}">${arr[i][0]}</button></li>`;
+                let listItem = `<li><button id="belist-item${i+1}">${arr[i][0]}</button></li>`;
                 exTable += listItem
             }
             
@@ -37,7 +40,7 @@ function createList (arr) {
         return exTable;
 }
 
-console.log(createList(advanced))
+
 //Function HTML syntax created from an  array and inserts into DOM //
 
 function displayList(arr){
@@ -47,7 +50,7 @@ function displayList(arr){
 
 // Create a list of the chosen exercise//
 function createExerciseList (arr,num){
-        let exerciseListTable = `<h3>You have Chosen ${arr[num][0]}! Good luck!</h3> <ol class="exerciseListTable">`;
+    let exerciseListTable = `<h3>You have Chosen ${arr[num][0]}! Good luck!</h3> <ol class="exerciseListTable">`;
         for (let i = 1 ; i < arr[num].length; i++){
             let listItem = `<li >${arr[num][i]}</li>`;
             exerciseListTable += listItem
@@ -63,8 +66,6 @@ function displayChosenEx(arr,num){
     return document.getElementById("display-exercise").innerHTML = exList;
 }
 
-
-
 // event listeners to display list of exercises //
 advancedButton.addEventListener("click", function(){displayList(advanced)});
 beginnerButton.addEventListener("click", function(){displayList(beginner)});
@@ -73,10 +74,68 @@ beginnerButton.addEventListener("click", function(){displayList(beginner)});
 // Add event listener to help display the exercise routine //
 
 document.addEventListener('click', event => {
-    if (event.target.matches("#list-item1" && ".advanced") ){
+    if(event.target.matches("#adlist-item1")){
         displayChosenEx(advanced,0);
-    } else if (event.target.matches("#list-item2")) {
+     } else if(event.target.matches("#adlist-item2")){
         displayChosenEx(advanced,1);
+     } else if(event.target.matches("#adlist-item3")){
+        displayChosenEx(advanced,2);
+     } else if(event.target.matches("#adlist-item4")){
+        displayChosenEx(advanced,3);
+     } else if(event.target.matches("#adlist-item5")){
+        displayChosenEx(advanced,4);
+     } else if(event.target.matches("#adlist-item6")){
+        displayChosenEx(advanced,5);
+     } else if(event.target.matches("#adlist-item7")){
+        displayChosenEx(advanced,6);
+     } else if(event.target.matches("#adlist-item8")){
+        displayChosenEx(advanced,7);
+     } else if(event.target.matches("#adlist-item9")){
+        displayChosenEx(advanced,8);
+     } else if(event.target.matches("#adlist-item10")){
+        displayChosenEx(advanced,9);
+     } else if(event.target.matches("#belist-item1")){
+        displayChosenEx(beginner,0);
+     } else if(event.target.matches("#belist-item2")){
+        displayChosenEx(beginner,1);
+     } else if(event.target.matches("#belist-item3")){
+        displayChosenEx(beginner,2);
+     } else if(event.target.matches("#belist-item4")){
+        displayChosenEx(beginner,3);
+     } else if(event.target.matches("#belist-item5")){
+        displayChosenEx(beginner,4);
+     } else if(event.target.matches("#belist-item6")){
+        displayChosenEx(beginner,5);
+     } else if(event.target.matches("#belist-item7")){
+        displayChosenEx(beginner,6);
+     } else if(event.target.matches("#belist-item8")){
+        displayChosenEx(beginner,7);
+     } else if(event.target.matches("#belist-item9")){
+        displayChosenEx(beginner,8);
+     } else if(event.target.matches("#belist-item10")){
+        displayChosenEx(beginner,9);
+     }
+    })
+
+    // Random //
+
+function randomChoice (){
+
+    // add html with two buttons to choose between beginner and advanced//
+
+    let chooseLevel =`<h3>Do you want a Beginner or Advanced workout?</h3><button id="random-beginner">Beginner</button><button id="random-advanced">Advanced</button>`;
+    document.getElementById("display-exercise").innerHTML = chooseLevel;
+}
+
+randomButton.addEventListener("click", function(){randomChoice()});
+
+document.addEventListener("click", event => {
+   if(event.target.matches("#random-beginner")){
+        let numOne = Math.floor(Math.random()*10);
+        displayChosenEx(beginner,numOne);
+    } else if (event.target.matches("#random-advanced")) {
+        let numTwo = Math.floor(Math.random()*10);
+        displayChosenEx(advanced, numTwo);
     }
-    
-  })
+})
+
